f71b830ccf30b669803872de791211daabe25673
ammo <ammo@noobuntu.lan>
2023-03-08 22:19:50 +0630

    timers

--- async/nexttick.js
+++ async/nexttick.js
@@ -1,1 +1,13 @@
 const events = require('node:events')
+
+function Emitter() {
+    let emitter = new events.EventEmitter()
+    process.nextTick(() => emitter.emit('start'))
+    return emitter
+}
+
+let myEmitter = new Emitter()
+
+myEmitter.on('start', () => {
+    console.log('Started')
+})
