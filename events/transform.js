1c0c437f3a29ce6a9f971cf25b0aec10d322b964
ammo <solidifyarmor@gmail.com>
2023-03-11 17:36:05 +0630

    transform stream

---
+++ events/transform.js
@@ -0,0 +1,9 @@
+const stream = require('node:stream')
+
+let converter = new stream.Transform()
+
+converter._transform = function (num, encoding, cb) {
+    this.push(String.fromCharCode(new Number(num)) + '\n')
+    cb()
+}
+process.stdin.pipe(converter).pipe(process.stdout)
